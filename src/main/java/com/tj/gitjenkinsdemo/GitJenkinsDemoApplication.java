da3f9aef89e2a7b09ad54bd9448d1a9091fea8b4
Tim Johnston <tedwardj11@gmail.com>
2019-10-14 21:28:32 -0400

    initialized spring project

---
+++ src/main/java/com/tj/gitjenkinsdemo/GitJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tj.gitjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitJenkinsDemoApplication.class, args);
+	}
+
+}
